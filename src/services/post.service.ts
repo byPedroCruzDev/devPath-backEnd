6c5dda5f660f507323f15bdabf8f6b6d2602f045
PedroCruz <contatocreed67@gmail.com>
2023-09-04 11:43:08 -0400

    .

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -2,23 +2,42 @@ import { Request, Response } from "express";
 import { AppDataSource } from "../data-source";
 import { User } from "../entities/user.entity";
 import { Post } from "../entities/post.entity";
+import { time } from "console";
 
 export class PostService {
   static async create(data: any, id: any) {
     const postRepository = AppDataSource.getRepository(Post);
     const userRepository = AppDataSource.getRepository(User);
-    console.log(data);
-    console.log(id);
 
     const postOwner = await userRepository
       .createQueryBuilder("user")
       .where("user.id = id", { id: id })
       .getOne();
+
+    const post: any = postRepository.create({
+      ...data,
+      author: postOwner,
+      creationDate: new Date(),
+      relations: { user: true, comment: true, like: true },
+    });
+    await postRepository.save(post);
+    delete post.author.password;
+    delete post.author.confirmPassword;
+
+    return post;
   }
 
   static async listOne(req: Request, res: Response) {}
 
-  static async listAll() {}
+  static async listAll(data: any): Promise<Response> {
+    const postRepository = AppDataSource.getRepository(Post);
+
+    const post: any = await postRepository.find({
+      relations: { author: true, like: true, comments: true },
+    });
+
+    return post;
+  }
 
   static async update(req: Request, res: Response) {}
 
