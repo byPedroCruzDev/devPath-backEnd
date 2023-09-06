380597a6af10895efffc8d1076792476817cd273
PedroCruz <contatocreed67@gmail.com>
2023-09-06 18:26:14 -0400

    adding delete and update user

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,12 @@
+import { hash } from "bcryptjs";
 import { AppDataSource } from "../data-source";
 import { User } from "../entities/user.entity";
-import { UserCreate, UserRead, UserReturn } from "../interface/user.interface";
+import {
+  UserCreate,
+  UserRead,
+  UserReadArray,
+  UserReturn,
+} from "../interface/user.interface";
 import {
   userReadSchema,
   userReadSchemaArray,
@@ -29,14 +35,40 @@ export class UserService {
     return user;
   }
 
-  static async listAll() {
+  static async listAll(): Promise<UserReadArray> {
     const userRepository = AppDataSource.getRepository(User);
 
     const allUsers = await userRepository.find();
     return userReadSchemaArray.parse(allUsers);
   }
 
-  static async update() {}
+  static async update(data: any, id: any): Promise<Response> {
+    const userRepository = AppDataSource.getRepository(User);
 
-  static async delete() {}
+    const user: any = await userRepository.findOneBy({
+      id: id,
+    });
+
+    if (data.password) {
+      data.password = await hash(data.password, 10);
+    }
+
+    const userUpdate = await userRepository.update(user!.id, data);
+
+    const { password, ...userWithoutPass } = (await userRepository.findOneBy({
+      id: id,
+    })) as any;
+
+    return user;
+  }
+
+  static async delete(id: any): Promise<any> {
+    const userRepository = AppDataSource.getRepository(User);
+
+    const user = await userRepository.delete({
+      id: id,
+    });
+
+    return user;
+  }
 }
